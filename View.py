84b8983cb969fb0c5eb55e8b0b98a33613b78965
Sergey Lunev <sergey@lunyov.ru>
2013-06-23 22:03:14 +0400

    Adding find use command which search for use statement and if dont find import it

--- View.py
+++ View.py
@@ -1,11 +1,31 @@
 import sublime
 import sublime_plugin
 import re
+import mmap
+import contextlib
 import os
-
 
 jump_to_action = ''
 
+def normalize_to_system_style_path(path):
+    if sublime.platform() == "windows":
+        path = re.sub(r"/([A-Za-z])/(.+)", r"\1:/\2", path)
+        path = re.sub(r"/", r"\\", path)
+    return path
+
+def find_symbol(symbol, window):
+    files = window.lookup_symbol_in_index(symbol)
+    namespaces = []
+    pattern = re.compile(b'^\s*namespace\s+([^;]+);', re.MULTILINE)
+
+    for file in files:
+        with open(normalize_to_system_style_path(file[0]), "rb") as f:
+            with contextlib.closing(mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)) as m:
+                for match in re.findall(pattern, m):
+                    namespaces.append([match.decode('utf-8') + "\\" + symbol, file[1]])
+                    break
+
+    return namespaces
 
 class SublimatedSymfonyViewCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -72,6 +92,66 @@ class SublimatedSymfonyViewCommand(sublime_plugin.TextCommand):
         bundle_folder = folder + os.sep + '..' + os.sep + '..' + os.sep
         controller = 'Controller' + os.sep + controller + 'Controller.php'
         return bundle_folder + controller
+
+class ImportUseCommand(sublime_plugin.TextCommand):
+    def run(self, edit, namespace):
+        print(namespace);
+
+        view = self.view
+        use_stmt = "use " + namespace + ";"
+
+        region = view.find(use_stmt.replace('\\', '\\\\'), 0)
+        if not region.empty():
+            return sublime.status_message('Use already exist !')
+
+        uses = []
+        regions = view.find_all(r"^(use\s+.+[;])", 0, '$1', uses)
+        uses.append(use_stmt)
+        uses = list(set(uses))
+        uses.sort()
+        uses = "\n".join(uses)
+
+        if len(regions) > 0:
+            region = regions[0]
+            for r in regions:
+                region = region.cover(r)
+
+            view.replace(edit, region, uses)
+            return sublime.status_message('Successfully imported' + namespace)
+
+        region = view.find(r"^\s*namespace\s+[\w\\]+[;{]", 0)
+        if not region.empty():
+            line = view.line(region)
+            view.insert(edit, line.end(), "\n" + uses)
+            return sublime.status_message('Successfully imported' + namespace)
+
+        region = view.find(r"<\?php", 0)
+        if not region.empty():
+            line = view.line(region)
+            view.insert(edit, line.end(), "\n" + uses)
+            return sublime.status_message('Successfully imported' + namespace)
+
+class FindUseCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        view = self.view
+        symbol = view.substr(view.word(view.sel()[0]))
+
+        if re.match(r"\w", symbol) is None:
+            return sublime.status_message('Not a valid symbol "%s" !' % symbol)
+
+        self.namespaces = find_symbol(symbol, view.window())
+
+        if len(self.namespaces) == 1:
+            self.view.run_command("import_use", {"namespace": self.namespaces[0][0]})
+
+        if len(self.namespaces) > 1:
+            view.window().show_quick_panel(self.namespaces, self.on_done)
+
+    def on_done(self, index):
+        if index == -1:
+            return
+
+        self.view.run_command("import_use", {"namespace": self.namespaces[index][0]})
 
 
 class SymfonyEvent(sublime_plugin.EventListener):
